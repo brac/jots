ae822d625f5266b4b1d07061011073f53c35f03a
Ben Bracamonte <ben.bracamonte@gmail.com>
2018-10-03 15:04:29 -0500

    basic styling

--- src/App.js
+++ src/App.js
@@ -24,40 +24,22 @@ class App extends Component {
           I am the header
         </header>
 
-        <main className="container" >
+        <main className="container-fluid pl-5 pr-5">
           <div className="row">
             <div className="col-md">
-              <Jot
-
-              />
-              <Jot
-
-              />
-              <Jot
-
-              />
-              <Jot
-
-              />
+              <Jot/>
+              <Jot/>
+              <Jot/>
+              <Jot/>
             </div>
             <div className="col-md">
-              <Jot
-
-              />
-              <Jot
-
-              />
+              <Jot/>
+              <Jot/>
             </div>
             <div className="col-md">
-              <Jot
-
-              />
-              <Jot
-
-              />
-              <Jot
-
-              />
+              <Jot/>
+              <Jot/>
+              <Jot/>
             </div>
           </div>
 
--- src/components/Jot.js
+++ src/components/Jot.js
@@ -3,8 +3,22 @@ import React from 'react';
 function Jot(props) {
   //jshint ignore:start
   return (
-    <div className="border border-secondary mt-3">
-      <h3>Why hello there</h3>
+    <div className="jot border border-secondary m-1">
+      <h4 className="jot-title">INSTALL GIT</h4>
+        <ul>
+          <li>
+            <h4>Install git on macOS with Homebrew</h4>
+            <p>$ brew install git</p>
+          </li>
+          <li>
+            <h4>Install git on Debian-based linux</h4>
+            <p>$ sudo apt-get install git</p>
+          </li>
+          <li>
+            <h4>Install git on Windows with Chocolatey</h4>
+            <p>$ choco install git</p>
+          </li>
+        </ul>
     </div>
   );
   //jshint ignore:end
